9841816ab987387e94becf44f91b2121a0272d73
reubenr <romandy29@gmail.com>
2014-09-23 00:07:42 -0700

    functional log in with mamp stack.

---
+++ app/src/main/java/com/edgecase/contested/library/UserFunctions.java
@@ -0,0 +1,86 @@
+package com.edgecase.contested.library;
+
+/**
+ * Created by reubenromandy on 9/21/14.
+ */
+
+import android.content.Context;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+public class UserFunctions {
+    private JSONParser jsonParser;
+    //URL of the contested API
+    private static String loginURL = "http://10.0.2.2:80/~reubenromandy/";
+    private static String registerURL = "http://10.0.2.2:80/~reubenromandy/";
+    private static String forpassURL = "http://10.0.2.2:80/";
+    private static String chgpassURL = "http://10.0.2.2:80/";
+    private static String login_tag = "login";
+    private static String register_tag = "register";
+    private static String forpass_tag = "forpass";
+    private static String chgpass_tag = "chgpass";
+    // constructor
+    public UserFunctions(){
+        jsonParser = new JSONParser();
+    }
+    /**
+     * Function to Login
+     **/
+    public JSONObject loginUser(String email, String password){
+        // Building Parameters
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", login_tag));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+        return json;
+    }
+    /**
+     * Function to change password
+     **/
+    public JSONObject chgPass(String newpas, String email){
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", chgpass_tag));
+        params.add(new BasicNameValuePair("newpas", newpas));
+        params.add(new BasicNameValuePair("email", email));
+        JSONObject json = jsonParser.getJSONFromUrl(chgpassURL, params);
+        return json;
+    }
+    /**
+     * Function to reset the password
+     **/
+    public JSONObject forPass(String forgotpassword){
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", forpass_tag));
+        params.add(new BasicNameValuePair("forgotpassword", forgotpassword));
+        JSONObject json = jsonParser.getJSONFromUrl(forpassURL, params);
+        return json;
+    }
+    /**
+     * Function to  Register
+     **/
+    public JSONObject registerUser(String fname, String lname, String email, String uname, String password){
+        // Building Parameters
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", register_tag));
+        params.add(new BasicNameValuePair("fname", fname));
+        params.add(new BasicNameValuePair("lname", lname));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("uname", uname));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL,params);
+        return json;
+    }
+    /**
+     * Function to logout user
+     * Resets the temporary data stored in SQLite Database
+     * */
+    public boolean logoutUser(Context context){
+        DatabaseHandler db = new DatabaseHandler(context);
+        db.resetTables();
+        return true;
+    }
+}
