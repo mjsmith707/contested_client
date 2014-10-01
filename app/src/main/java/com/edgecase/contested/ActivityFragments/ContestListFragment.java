e32704f1f2f3aecb818d8ad0fbc0f31d5cd2c794
reubenr <romandy29@gmail.com>
2014-10-01 09:28:22 -0700

    got create contest working but image request not happening

--- app/src/main/java/com/edgecase/contested/ActivityFragments/ContestListFragment.java
+++ app/src/main/java/com/edgecase/contested/ActivityFragments/ContestListFragment.java
@@ -36,7 +36,7 @@ public class ContestListFragment extends Fragment {
     private static final String TAG = ContestListFragment.class.getSimpleName();
 
     // Contests json url
-    private static final String url = "http://24.130.89.93:1234/";
+    private static final String url = "http://contested.grantkeller.org:1234/";
     private ProgressDialog pDialog;
     private List<Contest> contestList = new ArrayList<Contest>();
     private ListView listView;
