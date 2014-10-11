49b93983caa3c230c60c8b3522e5c3431df6ba8b
Grant Keller <gvk@sonic.net>
2014-10-10 21:03:23 -0700

    Fixed the contest fragment. The contests now display.

--- app/src/main/java/com/edgecase/contested/ActivityFragments/ContestFragment.java
+++ app/src/main/java/com/edgecase/contested/ActivityFragments/ContestFragment.java
@@ -1,13 +1,11 @@
 package com.edgecase.contested.ActivityFragments;
 
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +14,16 @@ import android.widget.TextView;
 
 import com.edgecase.contested.R;
 
-import java.io.ByteArrayOutputStream;
-
 /**
  * A simple {@link Fragment} subclass.
  *
  */
 public class ContestFragment extends Fragment {
-    private TextView contestName;
+    private TextView contestName ;
     private ImageButton entry;
     private String name;
     private Bitmap image;
-    private Bundle data;
+
 
     int mCurrentPage;
 
@@ -36,83 +32,39 @@ public class ContestFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         /** Getting the arguments to the Bundle object */
-        data = getArguments();
+        Bundle data = getArguments();
 
         /** Getting integer data of the key current_page from the bundle */
+        mCurrentPage = data.getInt("current_page", 0);
+        name = data.getString("CONTESTNAME");
+        image = setImage(data.getString("IMAGE"));
 
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View v = inflater.inflate(R.layout.contest_view_fragment, container, false);
-
+        View v = inflater.inflate(R.layout.contest_view_fragment, container,false);
+        contestName = (TextView) v.findViewById(R.id.contestName);
+        entry = (ImageButton) v.findViewById(R.id.entry);
+        setContent();
         return v;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mCurrentPage = data.getInt("current_page", 0);
-        name = data.getString("CONTESTNAME");
-        contestName = (TextView) getView().findViewById(R.id.contestName);
-        entry = (ImageButton) getView().findViewById(R.id.entry);
-       /* if (data.getString(("IMAGE")) != null) {
-            image = setImage(data.getString("IMAGE"));
-        }
-        else {*/
-
-
-        entry = (ImageButton) getView().findViewById(R.id.entry);
-        entry.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),
-                        ImagePickerActivity.class);
-                Log.e("String2", "String");
-                startActivityForResult(intent, 0);
-            }
-        });
-
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public Bitmap setImage(String image) {
+    public Bitmap setImage (String image){
 
         byte[] decodeImage = Base64.decode(image, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(decodeImage, 0, decodeImage.length);
 
     }
 
-    public void setContent() {
+    public void setContent (){
 
         contestName.setText(name);
         entry.setImageBitmap(image);
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        setContent();
-
-
-                Bundle extras = data.getExtras();
-
-
-                String filename = extras.getString("filename");
-        Bitmap bm = BitmapFactory.decodeFile(filename);
-        image = bm;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] b = baos.toByteArray();
-        String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
-
-        setContent();
-        }
-
-
-
-    }
+}
